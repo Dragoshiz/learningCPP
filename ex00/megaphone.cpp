3af5fd0b122a5fe11586f6d878ee8d786015d785
Dragos Imbrea <dragosimbrea@gmail.com>
2023-01-31 08:53:48 +0100

    have to review this

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: dimbrea <dimbrea@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 13:39:43 by dimbrea           #+#    #+#             */
-/*   Updated: 2023/01/30 14:37:26 by dimbrea          ###   ########.fr       */
+/*   Updated: 2023/01/31 08:52:18 by dimbrea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,14 @@
 
 int	main(int argc, char *argv[])
 {
-	int i, j;
-
-	i = 1;
 	if (argc >= 2)
 	{
-		while(argv[i])
+		for (int i = 1; argv[i]; i++)
 		{
-			j = 0;
-			while(argv[i][j])
-			{
+			for (int j = 0; argv[i][j]; j++)
 				std::cout << (char)toupper(argv[i][j]);
-				j++;
-			}
 			if (argv[i + 1])
 				std::cout << " ";
-			i++;
 		}
 		std::cout << std::endl;
 	}
