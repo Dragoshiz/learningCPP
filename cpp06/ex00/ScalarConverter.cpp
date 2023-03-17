dea0b9780dad0a999a0ea95b263a834b6a49f7c6
dimbrea <dragosimbrea@yahoo.it>
2023-03-17 21:23:41 +0100

    updating

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -27,31 +27,82 @@ bool isValidInt(const char *arg){
 	std::string str(arg);
 	if(str.length() > std::numeric_limits<int>::digits10 + 1)
 		return false;
-	int atoited = atoi(arg);
-	if(atoited == 0 && str != "0")
-		return false;
+	for (int i = 0; arg[i]; i++)
+	{
+		if (arg[i] == '-' && arg[i] != '\0' && i == 0)
+			i++;
+		if (isdigit(arg[i]) == 0)
+		{
+			if (arg[i] == 'f' && arg[i + 1] == '\0' && i != 0)
+				continue ;
+			return false;
+		}
+	}
 	return true;
 }
 
+int getInt(const char *arg){
+	
+}
 
+bool	scientificNotation(const char* arg){
+	std::string args = arg;
+	std::string arr[8] = {"inf","inff","nan","nanf","-inf","-inff", "+inf", "+inff"};
+	int i = 0;
+	while (i < 8 && args.compare(arr[i]) != 0)
+		i++;
+	if (i < 8)
+	{
+		std::cout << "float: " << ((i % 2 ==0)? arr[i + 1] : arr[i]) << "\n";
+		std::cout << "double: " << ((i % 2 ==0)? arr[i] : arr[i - 1]) << std::endl;
+		return true;
+	}
+	return false;
+}
+
+// bool isNum(std::string const &str)
+// {
+// 	auto it = str.begin();
+// 	while (it != str.end() && std::isdigit(*it)) {
+// 		it++;
+// 	}
+// 	return !str.empty() && it == str.end();
+// }
+
+void isChar(const char* arg){
+	if(isValidInt(arg)){
+		int num = atoi(arg);
+		if (num >= 32 && num < 127)
+			std::cout << "char: " << static_cast<char>(num) << "\n";
+		else if (num >= 0 && num < 32)
+			std::cout << "char: Non displayable"<< "\n";
+		else
+			std::cout << "char: impossible" << std::endl;
+	}
+}
+
+void printImps(void){
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: impossible" << std::endl;
+	std::cout << "double: impossible" << std::endl;
+	exit(1);
+}
 void ScalarConverter::convert(const char* arg){
-	int dec = 0;
-	if (isValidInt(arg))
-		dec = atoi(arg);
-	char characher = dec;
-	float ff = static_cast<float>(atof(arg));
-	double doubl = (atof(arg));
+	std::string argStr = arg;
+	if (!isValidInt(arg)){
+		printImps()
+	}
+	isChar(arg);
+	char **fuck = new char *;
+	double doubl = std::strtod(arg,fuck);//static_cast<float>(atof(arg));
+	float ff = static_cast<float>(doubl);
 	size_t dot = countDecimals(arg);
-
-	if (dec > 31 && dec < 127){ //this goes at the bottom;
-		std::cout << "char: " << characher << "\n";
+	if (!scientificNotation(arg)){
+		if ((doubl > std::numeric_limits<float>::max() || doubl < std::numeric_limits<float>::min()) && doubl > 16777215 && doubl < -16777216)
+			std::cout << "float: impossible" << std::endl;
+		
+		std::cout << "float: " << std::fixed << std::setprecision(dot) << ff << ((dot == 0) ? ".0f" : "f") << "\n";
+		std::cout << "double: " << std::fixed <<std::setprecision(dot)<< doubl << ((dot == 0) ? ".0" : "") << std::endl;
 	}
-	else{
-		std::cout << "char: Non displayable" << "\n";
-	}
-	std::cout <<"int: " << dec << std::endl;
-	std::cout << "float: "<< std::fixed << std::setprecision(dot) << ff<< "f" << "\n";
-	std::cout << "double: " << std::fixed <<std::setprecision(dot)<< doubl<< std::endl;
-
 }
 
