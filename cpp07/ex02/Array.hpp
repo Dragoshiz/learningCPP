56a55e634b185497f7d6898819c3a48b91421bf5
Ubuntu <dragosimbrea@gmail.com>
2023-03-30 16:12:16 +0000

    might finished

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -2,18 +2,21 @@
 #define ARRAY_HPP
 #include <iostream>
 #include <exception>
+#include <unistd.h>
+
 
 template<class T>
 class Array{
 	public:
-		Array(){
-			_array = new T[0];
-			_size = 0;
-		};
-		Array(unsigned int n){
+		Array():_array(new T[0]), _size(0){};
+
+		Array(const unsigned int n){
 			_array = new T[n];
 			_size = n;
+			for (int i=0; i < _size; i++)
+				_array[i] = 0;
 		};
+
 		Array(Array const &obj){
 			_array = new T[(obj._size)];
 			for (int i = 0; i < obj._size; i++){
@@ -21,14 +24,14 @@ class Array{
 			}
 			_size = obj._size;
 		};
-		int size(void)const{
-			return _size;
-		}
 		~Array(){
-			// delete[] _array;
+			std::cout <<"DESTRUCTOORR"<<std::endl;
+			delete[] _array;
 		};
 		Array<T>& operator=(Array const &obj){
 			if (this != &obj){
+				if(_array != NULL)
+					delete[] _array;
 				_array = new T[_size];
 				for(int i=0; i < obj._size; i++){
 					_array[i] = obj._array[i];
@@ -38,17 +41,20 @@ class Array{
 			return *this;
 		};
 		T &operator[](int i){
-			if(i > _size || i < 0)
+			if((i >= _size || i < 0) || (_size == 0 && i == 0))
 				throw(ArrayIndexOutOfBound());
-			return _array[i]; 
+			return _array[i];
 		}
 		class ArrayIndexOutOfBound: public std::exception{
 			const char * what() const throw(){
-				return "Array index out of bounds";
+				return "Array index out of bounds\n";
 			};
 		};
+		int size(void)const{
+			return _size;
+		}
 	private:
-		T		*_array;
+		T	*_array;
 		int	_size;
 };	
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,54 +1,74 @@
-
-#include <cstdlib>
 #include "Array.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
+int	main()
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	{
+		std::cout << "Making a class of type char\n\n";
+		Array<char> arr;
+		try{
+			std::cout << "Nothing in the char array\n" << arr[0] << std::endl;
+		}
+		catch(std::exception &e){
+			std::cerr << e.what() << std::endl;
+		}
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    // delete [] mirror;//
-    return 0;
+		usleep(90000);
+		std::cout << "Trying to acces index out of bounds" << std::endl;
+		try{
+			std::cout << arr[1] << std::endl ;
+		}
+		catch(std::exception &e){
+			std::cerr << e.what() << std::endl;
+		}
+		usleep(100000);
+		std::cout << "Making a class of type char with elements inside and copying it to the old one\n\n";
+		Array<char> arr2(10);
+		arr = arr2;
+		std::cout << "Trying again the same index as before\n";
+		try{
+			std::cout << "Nothing is displayed as it has a int 0" << arr[1];
+		}
+		catch(std::exception &e){
+			std::cerr << e.what() << std::endl;
+		}
+		usleep(10000);
+		std::cout << "Displaying the elements from the second array\n";
+		for (int i=0; i < arr2.size(); i++)
+			std::cout << "Element number: " << i << arr2[i] << "\n";
+		std::cout << std::endl;
+	}
+	{
+		std::cout << "Making a class of type int\n\n";
+		Array<int> arr;
+		try{
+			std::cout << "Nothing in the int array\n" << arr[0] << std::endl;
+		}
+		catch(std::exception &e){
+			std::cerr << e.what() << std::endl;
+		}
+		usleep(90000);
+		std::cout << "Trying to acces index out of bounds" << std::endl;
+		try{
+			std::cout << arr[1] << std::endl ;
+		}
+		catch(std::exception &e){
+			std::cerr << e.what() << std::endl;
+		}
+		usleep(100000);
+		std::cout << "Making a class of type int with elements inside and copying it to the old one\n\n";
+		Array<int> arr2(10);
+		arr = arr2;
+		std::cout << "Trying again the same index as before\n";
+		try{
+			std::cout << arr[1] << std::endl ;
+		}
+		catch(std::exception &e){
+			std::cerr << e.what() << std::endl;
+		}
+		usleep(10000);
+		std::cout << "Displaying the elements from the second array\n";
+		for (int i=0; i < arr2.size(); i++)
+			std::cout << "Element number: " << i << " " << arr2[i] << "\n";
+		std::cout << std::endl;
+	}
 }
